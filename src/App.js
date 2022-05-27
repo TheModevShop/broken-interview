79ce557a1d19d959667e2d86f2db34a62a1f3f54
vincent profeta <vprofeta12@gmail.com>
2022-05-27 10:12:10 -0500

    remove log

--- src/App.js
+++ src/App.js
@@ -9,23 +9,6 @@ var socket = io('https://interview-ry.herokuapp.com/')
 function App() {
   const [bids, setBids] = useState([])
   const [newBid, setNewBid] = useState(false)
-  useEffect(() => {
-    socket.on('item', bid => {
-      setBids([
-        {
-          id: bid.id,
-          tokenId: bid.tokenId,
-          image: bid.imageUrl,
-          favoritesCount: bid.favoritesCount,
-          chain: bid.assetContract.chain,
-          owner: bid.creator.address,
-          collectionName: bid.collection.name,
-          collectionImage: bid.collection.imageUrl,
-          price: bid.orderData.bestAsk.usdSpotPrice,
-        },
-      ])
-    })
-  }, [])
 
   return (
     <div className="App">
